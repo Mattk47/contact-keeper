367495e1482017188101c1b8cfe9b8dabe65097e
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-25 10:13:07 +0000

    complete register a user route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,54 @@
 const express = require('express');
 
 const router = express.Router();
-
+const User = require('../models/User')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 // @route       POST /api/users
 // @desc        Register a user
 // @access      Public
 
-router.post('/', (req, res) => res.send('Register user'))
+router.post('/', [check('name', 'Please enter your name').not().isEmpty(),
+check('email', 'Please enter a valid email').isEmail(),
+check('password', 'Please enter a password with at least 6 characters').isLength({ min: 6 })], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() })
+    }
+    const { name, email, password } = req.body;
 
+    try {
+        let user = await User.findOne({ email })
+        if (user) return res.status(400).send({ msg: 'user already exists' })
+
+        user = new User({
+            name, email, password
+        })
+
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        const payload = {
+            user: {
+                id: user.id,
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.send({ token })
+        })
+    } catch (error) {
+        console.error(error.msg)
+        res.status(500).send('server error')
+    }
+})
 
 module.exports = router;
