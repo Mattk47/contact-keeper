b7d47fc1dfa9043cf8806507acc8f41c7be0cdcd
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-25 10:09:13 +0000

    add middleware to enable request body extraction

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+const mongoose = require('mongoose');
 
-const app = express()
 
 app.get('/', (req, res) => { res.send('welcome to the contact keeper api!') })
+
+connectDB();
+
+app.use(express.json())
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`) })
