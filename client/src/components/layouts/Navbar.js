047cd074430bba77e146e4b568962c7e0dd1f002
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-25 14:10:44 +0000

    add navigation links to home and about pages

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
     return (
         <div className="navbar bg-primary">
             <h1><i className='fas fa-id-card-alt' /> Contact Keeper </h1>
+            <ul>
+                <li>
+                    <Link to='home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='about'>About</Link>
+                </li>
+            </ul>
         </div>
     )
 }
