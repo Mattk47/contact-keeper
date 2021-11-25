15d8d9ccfab8c65a14c716488a923ea8150a1850
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-25 14:09:34 +0000

    add routes to home and about pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import Navbar from './components/layouts/Navbar'
 import Home from './components/pages/Home'
+import About from './components/pages/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
     <Router>
       <div >
         <Navbar />
-        <div>
+        <div className='container'>
           <Routes>
             <Route exact path='/' element={<Home />} ></Route>
+            <Route exact path='/about' element={<About />} ></Route>
           </Routes>
         </div>
       </div>
