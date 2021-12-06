2d02100c676bec3748442ed26d352ee5bc27c6bc
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-06 15:16:19 +0000

    create contactForm component

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -8,7 +8,7 @@ const ContactForm = () => {
     const { name, email, phone, type } = contact
     return (
         <form>
-            <h2 className='text-primary'>Add contact</h2>
+            <h2 className='text-primary'>Add contact </h2>
             <input type='text' placeholder="name" name='name' value={name} onChange={onChange}></input>
         </form>
     )
