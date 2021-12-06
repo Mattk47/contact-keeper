bbaafce6dbc0cb519255a9e6e86e60b8f0f83e9f
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-06 16:09:53 +0000

    create contactreducer function

---
+++ client/src/context/contact/ContactReducer.js
@@ -0,0 +1,25 @@
+import {
+    GET_CONTACTS,
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_CONTACTS,
+    CLEAR_FILTER,
+    CONTACT_ERROR
+} from '../types';
+
+export default (state, action) => {
+    switch (action.type) {
+        case ADD_CONTACT:
+            return {
+                ...state,
+                contacts: [...state.contacts, action.payload]
+            }
+        default:
+            return state;
+    }
+
+}
