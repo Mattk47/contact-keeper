563efc3d515f04a48fbb0f92aec31c8ef981cb70
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-06 15:06:41 +0000

    wrap app with contactState

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import Navbar from './components/layouts/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ContactState from './context/contact/ContactState'
 
 function App() {
   return (
-    <Router>
-      <div >
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route exact path='/' element={<Home />} ></Route>
-            <Route exact path='/about' element={<About />} ></Route>
-          </Routes>
+    <ContactState>
+      <Router>
+        <div >
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route exact path='/' element={<Home />} ></Route>
+              <Route exact path='/about' element={<About />} ></Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ContactState>
   );
 }
 
