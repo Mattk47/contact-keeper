41f420a18708a2eddc4fd9c00c9ea56a20d4e8c4
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-06 15:08:05 +0000

    import contacts list component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
 
 const Home = () => {
     return (
-        <div>
-
+        <div className="grid-2">
+            <div>
+                {/* Contact form */}
+            </div>
+            <div>
+                <Contacts />
+            </div>
         </div>
     )
 }
