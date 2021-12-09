5c153574b133f59a7ece81242206bdf0287dd24f
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-09 11:25:45 +0000

    add edit & delete button functionality

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import ContactContext from '../../context/contact/ContactContext'
 
 const ContactForm = () => {
@@ -8,7 +8,19 @@ const ContactForm = () => {
 
     const contactContext = useContext(ContactContext)
 
+    const { addContact, current, clearCurrentContact, updateContact } = contactContext
+
     const { name, email, phone, type } = contact
+
+    useEffect(() => {
+        if (current !== null) {
+            setContact(current)
+        } else {
+            setContact({
+                name: '', email: '', phone: '', type: 'personal'
+            })
+        }
+    }, [contactContext, current])
 
     const onChange = e => {
         setContact({ ...contact, [e.target.name]: e.target.value })
@@ -16,15 +28,22 @@ const ContactForm = () => {
 
     const onSubmit = e => {
         e.preventDefault()
-        contactContext.addContact(contact)
+        if (current === null) {
+            addContact(contact)
+        } else {
+            updateContact(contact)
+        }
         setContact({
             name: '', email: '', phone: '', type: 'personal'
         })
     }
+    const clearAll = e => {
+        clearCurrentContact()
+    }
 
     return (
         <form onSubmit={onSubmit}>
-            <h2 className='text-primary'>Add contact </h2>
+            <h2 className='text-primary'>{current === null ? 'Add Contact' : 'Update Contact'}</h2>
             <input type='text'
                 placeholder="Name"
                 name='name' value={name}
@@ -38,12 +57,17 @@ const ContactForm = () => {
                 name='phone' value={phone}
                 onChange={onChange}></input>
             <h5>Contact Type</h5>
-            <input type='radio' name='type' value="personal" checked={type === 'personal'} />
+            <input type='radio' name='type' value="personal" checked={type === 'personal'} onChange={onChange} />
             Personal{'  '}
             <input type='radio' name='type' value="professional" checked={type === 'professional'} onChange={onChange} />
             Professional{' '}
             <div>
-                <input type='submit' value="Add Contact" className='btn btn-primary' onChange={onChange} />
+                <input type='submit' value={current === null ? 'Add contact' : 'Update contact'} className='btn btn-primary' />
+            </div>
+            <div>
+                {current && <div>
+                    <button className='btn btn-light btn-block' onClick={clearAll}>Clear</button>
+                </div>}
             </div>
 
         </form>
